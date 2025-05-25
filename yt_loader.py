69072845410adc068872bfe186d57f4a40b83841
ptmasher <glebbondarenko121@gmail.com>
2025-05-25 18:39:52 -0500

    1.1 - Directory selection

--- yt_loader.py
+++ yt_loader.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import ttk, messagebox, filedialog
+from os import getcwd, chdir
 from ttkthemes import ThemedTk
 import threading
 import yt_dlp
@@ -7,6 +8,15 @@ import yt_dlp
 
 def start_download():
     threading.Thread(target=download_video).start()
+
+def select_directory():
+    folder_path = filedialog.askdirectory()
+    dir_label['text'] = f"Current directory: {folder_path}"
+    root.update_idletasks()
+    try:
+        chdir(folder_path)
+    except Exception as e:
+        messagebox.showerror(f"Cannot access directory\n{e}")
 
 def download_video():
     try:
@@ -31,6 +41,8 @@ def download_video():
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 ydl.download([url])
             messagebox.showinfo("Success", "Video successfuly downloaded!")
+        except yt_dlp.DownloadError:
+            messagebox.showerror("Error", "Invalid URL")
         except Exception as e:
             messagebox.showerror("Error", f"Unexpected error occured:\n{str(e)}")
     finally:
@@ -42,13 +54,19 @@ root.title("YouTube loader")
 root.geometry('400x280')
 root.resizable(False, False)
 
-label = ttk.Label(root, text="Enter a YouTube video URL")
-label.pack(pady=10)
+head_label = ttk.Label(root, text="Enter a YouTube video URL")
+head_label.pack(pady=5)
 
 url_entry = ttk.Entry(root, width=50)
-url_entry.pack(pady=20)
+url_entry.pack(pady=10)
+
+dir_label = ttk.Label(root, text=f"Current directory: {getcwd()}")
+dir_label.pack(pady=10)
+
+directory_button = ttk.Button(root, text="Select directory", command=select_directory)
+directory_button.pack(pady=5)
 
 download_button = ttk.Button(root, text="Download", command=start_download)
-download_button.pack(pady=10)
+download_button.pack(pady=20)
 
 root.mainloop()
